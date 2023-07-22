e809278c451c52f8cc0cef6f37bdecf9aa64b373
hankmove <138128196+hankmove@users.noreply.github.com>
2023-07-22 10:16:42 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -1,8 +1,7 @@
 import { siteConfig } from './lib/site-config'
-
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '4949e87051ff4a03a6a329b5c941b65d',
+  rootNotionPageId: 'e2a5a67798a8443680190c976eecbcf4',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,8 +9,8 @@ export default siteConfig({
 
   // basic site info (required)
   name: '楚门之家',
-  domain: 'https://www.notion.so/4949e87051ff4a03a6a329b5c941b65d',
-  author: '柳叶刀',
+  domain: 'https://www.notion.so/e2a5a67798a8443680190c976eecbcf4',
+  author: '柳一白',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
